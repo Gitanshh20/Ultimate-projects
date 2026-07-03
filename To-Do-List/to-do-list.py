2485e8f394e6c0b2ea11ae189bbc5261948d9bff
Gitansh Agalcha <gitanshagalcha@gmail.com>
2026-07-03 20:13:38 +0530

    Update to-do-list.py

--- To-Do-List/to-do-list.py
+++ To-Do-List/to-do-list.py
@@ -1,26 +1,43 @@
-# To-Do-list
+# To-Do list 
 
-task = []
+print("To-Do-List | Code by Gitansh")
 
-while True:
-    print("\n1.Add Task\n2.Delete Task\n3.View Task\n4.Exit\n")
+def ToDoList():
+    tasks = []
     
-    choice = input("Enter Your Choice: ")
-    
-    if choice =='Add Task' or choice == '1':   
-        n = input("Enter Your Task: ")
-        task.append(n)
+    while True:
+        print("1.Add Tasks")
+        print("2.View Tasks")
+        print("3.Delete Tasks")
+        print("4.Exit")
         
-    elif choice == 'Delete Task' or choice == '2':
-        print(task)
-        delete = int(input("Enter Number of Task: ")) - 1
-        task.pop(delete)
+        choice = int(input("\nEnter Your Choice: "))
         
-    elif choice == 'View Task' or choice == '3':
-        print('\nYour Tasks: ',task)
+        if choice == 1 :
+            NoOfTask = int(input("Enter Number of Task You Want to Add: "))
+            for task in range(1, NoOfTask + 1):
+                task = input("Enter Your Task: ")
+                tasks.append(task)
+            print("Your All Task Added.")
             
-    elif choice == 'Exit' or choice == '4':
-        break
-    
-    else:
-        print("Something Went Wrong Try Again.....")
+        elif choice == 2:
+            if tasks == []:
+                print("No Task found...\n")
+            else:        
+                print(f"Here Your All Task -> {tasks}\n")
+            
+        elif choice == 3:
+            if tasks == []:
+                print("No Data Found..")
+            else:
+                print(f'Here Your Tasks -> {tasks}\n')
+                indexOfTask = int(input("Enter Number of Task You Want to delete: ")) - 1
+                tasks.pop(indexOfTask)
+                print('Your Task is Deleted.')
+                
+        elif choice == 4:
+            print("GoodBye!!")
+            break            
+            
+if __name__ == '__main__':
+    ToDoList()
